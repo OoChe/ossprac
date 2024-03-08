cfea7a6ce70dc71ebffcadbbfbb44a5d1e62e8a0
OoChe <135859243+OoChe@users.noreply.github.com>
2024-03-08 13:39:41 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    name = input("이름을 입력하세요: ")
+    student_id = input("학번을 입력하세요: ")
+    
+    print("입력된 정보:")
+    print("이름:", name)
+    print("학번:", student_id)
+
+if __name__ == "__main__":
+    main()
