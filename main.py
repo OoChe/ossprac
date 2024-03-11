eff5cc2a7e7ea205a0a9c723e3a9182b14e483e0
OoChe <jisukim924@gmail.com>
2024-03-11 12:54:51 +0900

    김지수 2022111941

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def main():
     name = input("이름을 입력하세요: ")
     student_id = input("학번을 입력하세요: ")
+    student_department = input("학과를 입력하세요: ")
     
     print("입력된 정보:")
     print("이름:", name)
     print("학번:", student_id)
+    print("학과: ", student_department)
 
 if __name__ == "__main__":
     main()
